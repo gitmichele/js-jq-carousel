427e9055601cce01a9b5fa25b44e34030f364a78
michele <micheleepedrotti@gmail.com>
2021-03-22 16:17:14 +0100

    next and previous buttons works

--- js/script.js
+++ js/script.js
@@ -1,8 +1,27 @@
+function nextClick(){
+
+    var currentImg = $('.img-container').find('img.js-active')
+    var nextImg = currentImg.next('img')
+
+    currentImg.removeClass('js-active')
+    nextImg.addClass('js-active')
+};
+
+function prevClick() {
+
+    var currentImg = $('.img-container').find('img.js-active')
+    var prevImg = currentImg.prev('img')
+
+    currentImg.removeClass('js-active')
+    prevImg.addClass('js-active')
+};
+
 function init(){
 
-
+    $('.arrow.right').click(nextClick);
+    $('.arrow.left').click(prevClick);
 };
 
 init();
 
-$('document').ready();
+$('document').ready(init);
