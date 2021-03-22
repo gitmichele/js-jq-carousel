ae4fb1da3fb104dab532140bbdccb2dec2a47293
michele <micheleepedrotti@gmail.com>
2021-03-22 16:54:00 +0100

    added 4 "dots" that change with imgs

--- js/script.js
+++ js/script.js
@@ -1,36 +1,58 @@
 function nextClick(){
 
     var currentImg = $('.img-container').find('img.js-active')
+    var currentDot = $('.dots').find('div.js-background')
 
-    if (currentImg.hasClass('js-last')){
+    if ((currentImg, currentDot).hasClass('js-last')){
 
         var nextImg = $('.img-container').find('img.js-first')
+        var nextDot = $('.dots').find('div.js-first')
+
         currentImg.removeClass('js-active')
         nextImg.addClass('js-active')
+
+        currentDot.removeClass('js-background')
+        nextDot.addClass('js-background')
     }
     else{
 
         var nextImg = currentImg.next('img')
+        var nextDot = currentDot.next('div')
+
         currentImg.removeClass('js-active')
         nextImg.addClass('js-active')
+
+        currentDot.removeClass('js-background')
+        nextDot.addClass('js-background')
     }
 };
 
 function prevClick() {
 
     var currentImg = $('.img-container').find('img.js-active')
+    var currentDot = $('.dots').find('div.js-background')
 
-    if (currentImg.hasClass('js-first')) {
+    if ((currentImg, currentDot).hasClass('js-first')) {
 
         var prevImg = $('.img-container').find('img.js-last')
+        var prevDot = $('.dots').find('div.js-last')
+
         currentImg.removeClass('js-active')
         prevImg.addClass('js-active')
+
+        currentDot.removeClass('js-background')
+        prevDot.addClass('js-background')
     }
     else {
 
         var prevImg = currentImg.prev('img')
+        var prevDot = currentDot.prev('div')
+        
         currentImg.removeClass('js-active')
         prevImg.addClass('js-active')
+
+        currentDot.removeClass('js-background')
+        prevDot.addClass('js-background')
     }
 };
 
